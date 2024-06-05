b0f013b2b850c1ac0e89056e7a4b2a5237369f3d
Tommy Brosman <tbrosman@microsoft.com>
2024-06-05 15:30:36 -0700

    Editing works, but no Fluid backing yet.

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import type React from 'react';
+import { useCallback, useRef, useState } from 'react';
 import './Grid.css';
 
 export function Grid() {
-    const itemBoard = [
-      [0, 0, 0, 1, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 1, 0, 0, 0, 0, 1, 0],
-      [1, 0, 0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 1, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0]];
-    const items = [...Array(64)].map((_, i) => {
-      const entry = itemBoard[Math.floor(i / 8)][i % 8];
-      const className = entry === 0 ? 'grid-item-black' : 'grid-item-white';
-      return (<div className={className} key={i}></div>);
-    });
-    return (
-      <div className="grid">
-        {items}
-      </div>
-    );
+  const [forceRender, setForceRender] = useState(false);
+  const initialItemBoard = [
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0]];
+  const itemBoardRef = useRef(initialItemBoard);
+
+  const items = [...Array(64)].map((_, i) => {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    const entry = itemBoardRef.current[y][x];
+    const className = entry === 0 ? 'grid-item-black' : 'grid-item-white';
+    const onClickCell = () => {
+      // Flip the color
+      itemBoardRef.current[y][x] = 1 - itemBoardRef.current[y][x];
+
+      // Tell React to re-render (note: won't be needed if the grid is passed down from a parent component)
+      setForceRender(!forceRender);
+    }
+
+    const key = `${x},${y}`;
+
+    // biome-ignore lint/a11y/useKeyWithClickEvents: Non-useful event.
+    return (<div className={className} key={key} onClick={onClickCell} />);
+  });
+  return (
+    <div className="grid">
+      {items}
+    </div>
+  );
 }
