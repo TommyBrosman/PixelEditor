44e23d436e9e76c25ab861583adc479b77100991
Tommy Brosman <tbrosman@microsoft.com>
2024-06-24 17:12:03 -0700

    Tests run from the command line and from the Test Explorer. It is possible to debug tests via a launch target. However, it is not yet possible to debug via Test Explorer.
    - Downgraded to mocha 4
    - Added ts-mocha to dependencies
    - Changed my test to use describe/it mocha methods

--- src/Grid.test.tsx
+++ src/Grid.test.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { expect } from 'chai';
 
 // eslint-disable-next-line import/no-unassigned-import
-import '@testing-library/jsdom'
+import '@testing-library/dom'
 import { render } from "@testing-library/react";
 import Grid from "./Grid";
 import { boardHeight, boardWidth } from "./store/InitialItemBoard";
+import { Provider } from "react-redux";
+import { store } from "./store/Store";
 
-test("Displays expected text and contains expected link", async (): Promise<void> => {
-	const { container } = render(<Grid/>);
-	const cells = Array.from(await container.querySelectorAll('.grid-item-black,.grid-item.white'));
-	expect(cells).length(boardWidth * boardHeight);
+describe("Tests for Grid", () => {
+	it("Displays expected text and contains expected link", async (): Promise<void> => {
+		const { container } = render(
+			<Provider store={store}>
+				<Grid/>
+			</Provider>);
+		const cells = Array.from(await container.querySelectorAll('.grid-item-black,.grid-item.white'));
+		expect(cells).length(boardWidth * boardHeight);
+	});
 });
