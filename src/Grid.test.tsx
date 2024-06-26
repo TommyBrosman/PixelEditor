a6d3ad9416e37e44e70f70d3f3c006004a73ae9f
Tommy Brosman <tbrosman@microsoft.com>
2024-06-26 16:55:55 -0700

    Added a slightly more interesting test.

--- src/Grid.test.tsx
+++ src/Grid.test.tsx
@@ -1,22 +1,49 @@
 import React from "react";
-import { expect } from 'chai';
+import { expect } from "chai";
 
 // eslint-disable-next-line import/no-unassigned-import
-import '@testing-library/dom'
-import { render } from "@testing-library/react";
+import "@testing-library/dom"
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import Grid from "./Grid";
 import { boardHeight, boardWidth, initialItemBoard } from "./store/InitialItemBoard";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/Store";
 
 describe("Tests for Grid", () => {
+	const emptyItemBoard: number[][] = [
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
 	it("Displays expected text and contains expected link", async (): Promise<void> => {
 		const store = setupStore({ app: { isLoaded: true, itemBoard: initialItemBoard } });
 		const { container } = render(
 			<Provider store={store}>
 				<Grid/>
 			</Provider>);
-		const cells = Array.from(await container.querySelectorAll('.grid-item-black,.grid-item-white'));
+		const cells = Array.from(container.querySelectorAll('.grid-item-black,.grid-item-white'));
 		expect(cells).length(boardWidth * boardHeight);
+	});
+
+	it("Toggles a cell", async (): Promise<void> => {
+		const store = setupStore({ app: { isLoaded: true, itemBoard: emptyItemBoard } });
+		const { container } = render(
+			<Provider store={store}>
+				<Grid/>
+			</Provider>);
+		const blackCellsBefore = Array.from(container.querySelectorAll('.grid-item-black'));
+		const whiteCellsBefore = Array.from(container.querySelectorAll('.grid-item-white'));
+		fireEvent.click(blackCellsBefore[0]);
+
+		waitFor(() => {
+			const whiteCellsAfter = Array.from(container.querySelectorAll('.grid-item-white'));
+			expect(whiteCellsBefore).length(whiteCellsAfter.length + 1);
+		});
 	});
 });
